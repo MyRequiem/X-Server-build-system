98dcca94732d5877d4ee2ca37d7fbc63be1e4fb7
MyRequiem <mailto:mrvladislavovich@gmail.com>
2017-02-19 12:12:14 +0400

    modified additional-scripts/strip-binaries.sh

--- additional-scripts/strip-binaries.sh
+++ additional-scripts/strip-binaries.sh
@@ -3,9 +3,8 @@
 # strip binaries:
 echo -e "${GREEN}Strip binaries in ${BROWN}${PKG}/${CDEF}"
 
-find "${PKG}" -print0 | xargs -0 file | grep "executable" | grep ELF | \
-    cut -f 1 -d : | xargs strip --strip-unneeded 2> /dev/null
-find "${PKG}" -print0 | xargs -0 file | grep "shared object" | grep ELF | \
-    cut -f 1 -d : | xargs strip --strip-unneeded 2> /dev/null
-find "${PKG}" -print0 | xargs -0 file | grep "current ar archive" | grep ELF | \
-    cut -f 1 -d : | xargs strip -g 2> /dev/null
+find "${PKG}" -type f -print0 | xargs -0 file | grep -e "executable" \
+    -e "shared object" | grep ELF | cut -f 1 -d : | \
+    xargs strip --strip-unneeded 2> /dev/null
+find "${PKG}" -type f -print0 | xargs -0 file | grep "current ar archive" | \
+    grep ELF | cut -f 1 -d : | xargs strip -g 2> /dev/null
