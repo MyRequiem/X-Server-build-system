0b7c32a52053ae1781e1c69d6566088b5b9cd4d5
MyRequiem <mailto:mrvladislavovich@gmail.com>
2017-02-21 21:59:14 +0400

    |     under construction: added part of the necessary files

--- additional-scripts/settings.sh
+++ additional-scripts/settings.sh
@@ -11,7 +11,7 @@ export TAG="myreq"
 # install package after build (true, false)
 export INSTALL_AFTER_BUILD="false"
 # check package version
-export CHECK_PACKAGE_VERSION="false"
+export CHECK_PACKAGE_VERSION="true"
 # only download src (without build)
 export ONLY_DOWNLOAD="false"
 
