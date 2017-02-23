a64dd49747c85fe678cb00615035547807c76853
MyRequiem <mailto:mrvladislavovich@gmail.com>
2017-02-23 18:24:08 +0400

    under construction: added misc/libva build scripts

---
+++ src/misc/libva/build.sh
@@ -0,0 +1,76 @@
+#!/bin/sh
+
+PKGNAME="libva"
+
+if [[ "${CHECK_PACKAGE_VERSION}" == "true" ]]; then
+    # check latest release version
+    echo -en "${GREY}Check ${CYAN}${PKGNAME}${GREY} latest release:${CDEF} "
+    DOWNLOAD="https://www.freedesktop.org/software/vaapi/releases/libva/"
+    VERSION=$(wget -q -O - "${DOWNLOAD}" | grep '<a href="libva-' | \
+        grep ".tar.bz2<" | cut -d \" -f 8 | cut -d - -f 2 | rev | \
+        cut -d . -f 3- | rev | sort -V | tail -n 1)
+    SOURCE="${PKGNAME}-${VERSION}.tar.bz2"
+    echo "${VERSION}"
+
+    # download source archive if does not exist
+    if ! [ -r "${SOURCE}" ]; then
+        echo -e "${YELLOW}Downloading ${SOURCE} source archive${CDEF}"
+        wget "${DOWNLOAD}/${SOURCE}"
+    fi
+else
+    SOURCE=$(ls "${PKGNAME}-"*.tar.?z*)
+    VERSION=$(echo "${SOURCE}" | rev | cut -d - -f 1 | cut -d . -f 3- | rev)
+fi
+
+[[ "${ONLY_DOWNLOAD}" == "true" ]] && exit 0
+
+CWD=$(pwd)
+TMP="${TMP}/misc"
+PKG="${TMP}/package-${PKGNAME}"
+
+rm -rf "${PKG}"
+mkdir -p "${PKG}"
+cd "${TMP}" || exit 1
+rm -rf "${PKGNAME}-${VERSION}"
+tar xvf "${CWD}/${SOURCE}"
+cd "${PKGNAME}-${VERSION}" || exit 1
+. "${CWDD}"/additional-scripts/setperm.sh
+
+# these fix a mandatory check for wayland-scanner:
+mkdir -p m4
+autoreconf -fi
+
+CFLAGS="${SLKCFLAGS}" \
+CXXFLAGS="${SLKCFLAGS}" \
+./configure \
+    --prefix=/usr \
+    --libdir=/usr/lib"${LIBDIRSUFFIX}" \
+    --sysconfdir=/etc \
+    --localstatedir=/var \
+    --mandir=/usr/man \
+    --docdir=/usr/doc/"${PKGNAME}"-"${VERSION}" \
+    --enable-shared \
+    --disable-static \
+    --enable-dummy-driver \
+    --build="${ARCH}"-slackware-linux || exit 1
+
+make "${NUMJOBS}" || make || exit 1
+make install-strip DESTDIR="${PKG}" || exit 1
+
+. "${CWDD}"/additional-scripts/strip-binaries.sh
+. "${CWDD}"/additional-scripts/copydocs.sh
+. "${CWDD}"/additional-scripts/compressmanpages.sh
+
+mkdir -p "${PKG}/install"
+cat "${CWD}/slack-desc" > "${PKG}/install/slack-desc"
+
+cd "${PKG}" || exit 1
+mkdir -p "${OUTPUT}/misc"
+BUILD=$(cat "${CWDD}/build/${PKGNAME}")
+PKG="${OUTPUT}/misc/${PKGNAME}-${VERSION}-${ARCH}-${BUILD}_${TAG}.${EXT}"
+rm -f "${PKG}"
+makepkg -l y -c n "${PKG}"
+
+if [[ "${INSTALL_AFTER_BUILD}" == "true" ]]; then
+    upgradepkg --install-new --reinstall "${PKG}"
+fi
