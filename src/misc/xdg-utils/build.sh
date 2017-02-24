ba5cec3c6d3d55733e79ca7f51405a7389bfaf23
MyRequiem <mailto:mrvladislavovich@gmail.com>
2017-02-24 14:25:37 +0400

    under construction: added scripts for building misc/xdg-utils

---
+++ src/misc/xdg-utils/build.sh
@@ -0,0 +1,63 @@
+#!/bin/sh
+
+PKGNAME="xdg-utils"
+
+if [[ "${CHECK_PACKAGE_VERSION}" == "true" ]]; then
+    # check latest release version
+    echo -en "${GREY}Check ${CYAN}${PKGNAME}${GREY} latest release:${CDEF} "
+    DOWNLOAD="https://portland.freedesktop.org/download/"
+    VERSION=$(wget -q -O - "${DOWNLOAD}" | grep 'href="xdg-utils-' | \
+        grep '.tar.gz"' | grep -v "\-rc" | cut -d \" -f 8 | rev | \
+        cut -d - -f 1 | cut -d . -f 3- | rev | sort -V | tail -n 1)
+    SOURCE="${PKGNAME}-${VERSION}.tar.gz"
+    echo "${VERSION}"
+
+    # download source archive if does not exist
+    if ! [ -r "${SOURCE}" ]; then
+        echo -e "${YELLOW}Downloading ${SOURCE} source archive${CDEF}"
+        wget "${DOWNLOAD}${SOURCE}"
+    fi
+else
+    SOURCE=$(ls "${PKGNAME}-"*.tar.?z*)
+    VERSION=$(echo "${SOURCE}" | rev | cut -d - -f 1 | cut -d . -f 3- | rev)
+fi
+
+[[ "${ONLY_DOWNLOAD}" == "true" ]] && exit 0
+
+CWD=$(pwd)
+TMP="${TMP}/misc"
+PKG="${TMP}/package-${PKGNAME}"
+
+rm -rf "${PKG}"
+mkdir -p "${PKG}"
+cd "${TMP}" || exit 1
+rm -rf "${PKGNAME}-${VERSION}"
+tar xvf "${CWD}/${SOURCE}"
+cd "${PKGNAME}-${VERSION}" || exit 1
+. "${CWDD}"/additional-scripts/setperm.sh
+
+./configure \
+  --prefix=/usr \
+  --sysconfdir=/etc \
+  --localstatedir=/var \
+  --mandir=/usr/man || exit 1
+
+make "${NUMJOBS}" || make || exit 1
+make install DESTDIR="${PKG}" || exit 1
+
+. "${CWDD}"/additional-scripts/copydocs.sh
+. "${CWDD}"/additional-scripts/compressmanpages.sh
+
+mkdir -p "${PKG}/install"
+cat "${CWD}/slack-desc" > "${PKG}/install/slack-desc"
+
+cd "${PKG}" || exit 1
+mkdir -p "${OUTPUT}/misc"
+BUILD=$(cat "${CWDD}/build/${PKGNAME}")
+PKG="${OUTPUT}/misc/${PKGNAME}-${VERSION}-${ARCH}-${BUILD}_${TAG}.${EXT}"
+rm -f "${PKG}"
+makepkg -l y -c n "${PKG}"
+
+if [[ "${INSTALL_AFTER_BUILD}" == "true" ]]; then
+    upgradepkg --install-new --reinstall "${PKG}"
+fi
