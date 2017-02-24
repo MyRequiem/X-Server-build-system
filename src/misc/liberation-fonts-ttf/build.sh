c47a803580acd9c16e11e2f9e9e5bda136ecdf85
MyRequiem <mailto:mrvladislavovich@gmail.com>
2017-02-24 18:48:43 +0400

    under construction: added all scripts for building misc module

---
+++ src/misc/liberation-fonts-ttf/build.sh
@@ -0,0 +1,67 @@
+#!/bin/sh
+
+PKGNAME="liberation-fonts-ttf"
+
+if [[ "${CHECK_PACKAGE_VERSION}" == "true" ]]; then
+    # check latest release version
+    echo -en "${GREY}Check ${CYAN}${PKGNAME}${GREY} latest release:${CDEF} "
+    DOWNLOAD="https://fedorahosted.org/releases/l/i/liberation-fonts/"
+    VERSION=$(wget -q -O - "${DOWNLOAD}" | \
+        grep 'href="liberation-fonts-ttf-' | grep -v -e ".beta" -e ".devel" | \
+        grep '.tar.gz"' | cut -d \" -f 2 | rev | cut -d - -f 1 | \
+        cut -d . -f 3- | rev | sort -V | tail -n 1)
+    SOURCE="${PKGNAME}-${VERSION}.tar.gz"
+    echo "${VERSION}"
+
+    # download source archive if does not exist
+    if ! [ -r "${SOURCE}" ]; then
+        echo -e "${YELLOW}Downloading ${SOURCE} source archive${CDEF}"
+        wget "${DOWNLOAD}/${SOURCE}"
+    fi
+else
+    SOURCE=$(ls "${PKGNAME}-"*.tar.?z*)
+    VERSION=$(echo "${SOURCE}" | rev | cut -d - -f 1 | cut -d . -f 3- | rev)
+fi
+
+[[ "${ONLY_DOWNLOAD}" == "true" ]] && exit 0
+
+CWD=$(pwd)
+TMP="${TMP}/misc"
+PKG="${TMP}/package-${PKGNAME}"
+
+rm -rf "${PKG}"
+mkdir -p "${PKG}"
+cd "${TMP}" || exit 1
+rm -rf "${PKGNAME}-${VERSION}"
+tar xvf "${CWD}/${SOURCE}"
+cd "${PKGNAME}-${VERSION}" || exit 1
+. "${CWDD}"/additional-scripts/setperm.sh
+
+TTF="${PKG}/usr/share/fonts/TTF/"
+mkdir -p "${TTF}"
+find . -type f -name "*.ttf" -exec cp -a {} "${TTF}" \;
+
+. "${CWDD}"/additional-scripts/copydocs.sh
+
+FONTS="${PKG}/etc/fonts"
+mkdir -p "${FONTS}"/conf.{d,avail}
+cat "${CWD}/60-liberation.conf" > "${FONTS}/conf.avail/60-liberation.conf"
+(
+    cd "${FONTS}/conf.d" || exit 1
+    ln -sf ../conf.avail/60-liberation.conf 60-liberation.conf
+)
+
+mkdir -p "${PKG}/install"
+cat "${CWD}/slack-desc" > "${PKG}/install/slack-desc"
+cat "${CWD}/doinst.sh" > "${PKG}/install/doinst.sh"
+
+cd "${PKG}" || exit 1
+mkdir -p "${OUTPUT}/misc"
+BUILD=$(cat "${CWDD}/build/${PKGNAME}")
+PKG="${OUTPUT}/misc/${PKGNAME}-${VERSION}-noarch-${BUILD}_${TAG}.${EXT}"
+rm -f "${PKG}"
+makepkg -l y -c n "${PKG}"
+
+if [[ "${INSTALL_AFTER_BUILD}" == "true" ]]; then
+    upgradepkg --install-new --reinstall "${PKG}"
+fi
