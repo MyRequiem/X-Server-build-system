7696fd2b9e08f996b9a24329f31fe81b1cff3889
MyRequiem <mailto:mrvladislavovich@gmail.com>
2017-03-03 11:04:55 +0400

    modified settings.sh

--- settings.sh
+++ settings.sh
@@ -4,24 +4,24 @@
 # temp directory for building packages
 export TEMP="/tmp/x-build"
 # output for packages
-export OUTPUT="/root/src/x-packages"
+export OUTPUT="/tmp/x"
 # package extension
 export EXT="txz"
-# tar fot package
+# tag for package
 export TAG="myreq"
 # branch mesa package [https://www.mesa3d.org/]
 export MESA_BRANCH="13"
 # build package only if it is not in OUTPUT
-export BUILD_ONLY_NOT_EXIST="true"
+export BUILD_ONLY_NOT_EXIST="false"
 # install package after build
-export INSTALL_AFTER_BUILD="false"
+export INSTALL_AFTER_BUILD="true"
 # check package version
 export CHECK_PACKAGE_VERSION="true"
 # only download source code (without build)
 export ONLY_DOWNLOAD="false"
 # ========================== End of settings ===================================
 
-# if ONLY_DOWNLOAD="true" variables BUILD_ONLY_NOT_EXIST and
-# CHECK_PACKAGE_VERSION must be set "true"
+# if ONLY_DOWNLOAD == "true" variable CHECK_PACKAGE_VERSION must be set "true"
+# and BUILD_ONLY_NOT_EXIST must be set "false"
 [[ "${ONLY_DOWNLOAD}" == "true" ]] && export CHECK_PACKAGE_VERSION="true" &&
     BUILD_ONLY_NOT_EXIST="false"
