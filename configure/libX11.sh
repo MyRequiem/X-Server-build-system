f03c978798d4c248de015901f9762849f2a3dc63
MyRequiem <mailto:mrvladislavovich@gmail.com>
2017-07-03 14:20:27 +0400

    update to Slackware-14.2

--- configure/libX11.sh
+++ configure/libX11.sh
@@ -4,11 +4,11 @@ CFLAGS="${SLKCFLAGS}" \
 CXXFLAGS="${SLKCFLAGS}" \
 ./configure \
     --prefix=/usr \
-    --libdir=/usr/lib"${LIBDIRSUFFIX}" \
+    --libdir="/usr/lib${LIBDIRSUFFIX}" \
     --sysconfdir=/etc \
     --localstatedir=/var \
     --infodir=/usr/info \
     --mandir=/usr/man \
-    --docdir=/usr/doc/"${PKGNAME}-${VERSION}" \
+    --docdir="/usr/doc/${PKGNAME}-${VERSION}" \
     --disable-static \
-    --build="${ARCH}"-slackware-linux
+    --build="${ARCH}-slackware-linux"
