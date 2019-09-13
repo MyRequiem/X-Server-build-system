924d4ec62a740ad1e28ee09c79c9fc9729991897
Vladimir <mrvladislavovich@gmail.com>
2019-09-13 20:15:18 +0400

    font-misc-ethiopic added

---
+++ doinst.sh/font-misc-ethiopic.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+if [[ -x /usr/bin/mkfontdir || -x /usr/X11R6/bin/mkfontdir ]]; then
+    (
+        cd /usr/share/fonts/OTF || exit 1
+        mkfontscale .
+        mkfontdir .
+
+        cd /usr/share/fonts/TTF || exit 1
+        mkfontscale .
+        mkfontdir .
+    )
+fi
+
+if [ -x /usr/bin/fc-cache ]; then
+    /usr/bin/fc-cache -f
+fi
